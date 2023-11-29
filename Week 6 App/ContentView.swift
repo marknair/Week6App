f08c98c73d908529ca03cc8ba600707c10b2f71a
Mark Nair <marknair@me.com>
2023-11-29 15:56:58 -0600

    Initial Commit

---
+++ Week 6 App/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Week 6 App
+//
+//  Created by Mark Nair on 11/29/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Week 6 App/Week_6_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  Week_6_AppApp.swift
+//  Week 6 App
+//
+//  Created by Mark Nair on 11/29/23.
+//
+
+import SwiftUI
+
+@main
+struct Week_6_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
